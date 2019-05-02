93aafc586c06628c58c7b7f87249edd61cddd634
GoingLight <48763973+GoingLight@users.noreply.github.com>
2019-05-02 10:50:02 +0800

    Add files via upload

---
+++ anxiety.js
@@ -0,0 +1,60 @@
+var i=0;
+function readMe(){
+  if(!i){
+    document.getElementById("two").style.display = "inline";
+    document.getElementById("one").style.display = "none";
+    document.getElementById("three").innerHTML = "Read Less";
+    i=1;
+  }
+  else{
+    document.getElementById("two").style.display = "none";
+    document.getElementById("one").style.display = "inline";
+    document.getElementById("three").innerHTML = "Read More";
+  i=0;
+  }
+}
+var i=0;
+function readMe2(){
+  if(!i){
+    document.getElementById("sdua").style.display = "inline";
+    document.getElementById("ssatu").style.display = "none";
+    document.getElementById("stiga").innerHTML = "Read Less";
+    i=1;
+  }
+  else{
+    document.getElementById("sdua").style.display = "none";
+    document.getElementById("ssatu").style.display = "inline";
+    document.getElementById("stiga").innerHTML = "Read More";
+  i=0;
+  }
+}
+var i=0;
+function readMe3(){
+  if(!i){
+    document.getElementById("bdua").style.display = "inline";
+    document.getElementById("bsatu").style.display = "none";
+    document.getElementById("btiga").innerHTML = "Read Less";
+    i=1;
+  }
+  else{
+    document.getElementById("bdua").style.display = "none";
+    document.getElementById("bsatu").style.display = "inline";
+    document.getElementById("btiga").innerHTML = "Read More";
+  i=0;
+  }
+}
+var i=0;
+function readMe4(){
+  if(!i){
+    document.getElementById("dua").style.display = "inline";
+    document.getElementById("satu").style.display = "none";
+    document.getElementById("tiga").innerHTML = "Read Less";
+    i=1;
+  }
+  else{
+    document.getElementById("dua").style.display = "none";
+    document.getElementById("satu").style.display = "inline";
+    document.getElementById("tiga").innerHTML = "Read More";
+  i=0;
+  }
+}
